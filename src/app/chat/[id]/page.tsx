dd4fc4418bd3ce9b297b7c7c0a6f6c171f19d731
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-12-27 19:03:15 -0400

    Refactor chat page and chat card component

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,7 +1,5 @@
 import { Chat } from '@components/server'
 
 export default function ChatPage (): JSX.Element {
-  return (
-    <Chat />
-  )
+  return <Chat />
 }
--- src/app/components/server/chat-card.tsx
+++ src/app/components/server/chat-card.tsx
@@ -1,5 +1,6 @@
 import { Avatar } from '@components/server/ui'
 import { DeleteChat } from '@components/client/chat'
+
 import Link from 'next/link'
 interface ChatProps {
   name: string
@@ -13,12 +14,12 @@ function Chat ({ name, href }: ChatProps): JSX.Element {
     <div className='flex flex-row gap-x-2 justify-between'>
       <Link
         href={href}
-        className='flex flex-row items-center hover:bg-zinc-500 rounded-xl p-1 px-2 cursor-pointer'
+        className='flex flex-row items-center hover:bg-zinc-500 rounded-xl p-1 px-2 cursor-pointer truncate'
       >
         <Avatar>{getFirstCapitalize(name)}</Avatar>
-        <span className='ml-2 font-bold text-lg truncate'>{name}</span>
+        <span className='ml-2 font-bold text-lg'>{name}</span>
       </Link>
-      <DeleteChat id={href} />
+      <DeleteChat id={href.replace('/chat/', '')} />
     </div>
   )
 }
